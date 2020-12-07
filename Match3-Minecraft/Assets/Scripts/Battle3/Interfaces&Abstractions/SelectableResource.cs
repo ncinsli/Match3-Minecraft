f184c2054445aadb317aaa0f48c4043e3c52d29b
Василий Гайсен <56553344+ncinsli@users.noreply.github.com>
2020-12-07 16:47:09 +0500

    Refactored and optimized a little bit
    Selector
    SelectableResource

--- Match3-Minecraft/Assets/Scripts/Battle3/Interfaces&Abstractions/SelectableResource.cs
+++ Match3-Minecraft/Assets/Scripts/Battle3/Interfaces&Abstractions/SelectableResource.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class SelectableResource : MonoBehaviour{
-    [SerializeField] public InventoryElement info; //Сам элемент, который хранит выбранный ресурс
+    [SerializeField] private List<SelectableResource> neighbours = new List<SelectableResource>();
+    [SerializeField] private InventoryElement info; //Сам элемент, который хранит выбранный ресурс
+    public event Action<SelectableResource> OnSelect;
+    private Color originColor;
+    private bool isSelected;
+    private void Awake() => originColor = GetComponent<SpriteRenderer>().color;
+
     public Vector2 gridPosition{ //В проекте есть свой класс сетки, этот метод вызывает этот класс и возвращает позицию  
         get{ 
             Vector2 p = Vector2.zero;
@@ -14,7 +21,35 @@ public class SelectableResource : MonoBehaviour{
             return p / BattleGrid.instance.cellSize;
         }
     }
+    private void OnMouseEnter(){
+        if (isSelected) return;
+        OnSelect?.Invoke(this);
+    }
+    private void OnMouseDown() {
+        if (isSelected) return;
+        OnSelect?.Invoke(this);
+    }
+
 //  @temp!
-    public void Select() => GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 1f);
-    public void Deselect() => GetComponent<SpriteRenderer>().color = new Color(1f, 0f, 1f, 1f);
+    public void Select(){ 
+        GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 1f);
+        isSelected = true;
+    }
+
+    public void Deselect(){ 
+        GetComponent<SpriteRenderer>().color = originColor;
+        isSelected = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col){
+        if (col.gameObject.TryGetComponent<SelectableResource>(out var other)){
+            if (neighbours.Contains(other)) return;
+            neighbours.Add(other);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col){
+        if (col.gameObject.TryGetComponent<SelectableResource>(out var other)){
+            neighbours.Remove(other);
+        }
+    }
 }
--- Match3-Minecraft/Assets/Scripts/Battle3/Selector.cs
+++ Match3-Minecraft/Assets/Scripts/Battle3/Selector.cs
@@ -1,58 +1,42 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Selector : MonoBehaviour{
     [SerializeField] private List<SelectableResource> resources; 
-    protected float mouseDeltaModifier = 0.15f; //Так как дельта мыши высчитывается странно, введём магическое число
-    protected Vector2 mousePositionSinceClick;
-    protected RaycastHit hit;
-    public Transform t;
-    private void Awake() => resources = new List<SelectableResource>();
-
-    private void Update(){  //ScreenToWorldPoint - дорогая операция, потому кэшируем её, а потом меняем через Mouse Axis
-        if (Input.GetKeyDown(KeyCode.Mouse0)) BeforeSelect();
-        if (Input.GetKey(KeyCode.Mouse0)) TrySelect();
-        if (Input.GetKeyUp(KeyCode.Mouse0)) AfterSelect();
-        t.position = mousePositionSinceClick;
-        t.position = new Vector3(t.position.x, t.position.y, 3);
+    private List<SelectableResource> selectedResources = new List<SelectableResource>();
+    private float mouseDeltaModifier = 0.15f; //Так как дельта мыши высчитывается странно, введём магическое число
+    private Vector2 mousePositionSinceClick;
+    private bool canSelect;
+    private RaycastHit hit;
+    private void Awake(){ 
+        foreach (var resource in resources)
+            resource.OnSelect += SelectResource;
     }
-
-    private void TrySelect(){
-        mousePositionSinceClick += Time.deltaTime * 40f * new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        Vector2 mouse = mousePositionSinceClick;
-        Vector3 direction = new Vector3(mouse.x, mouse.y, 0f); //Чтобы не было наклона луча
-            Debug.DrawRay(mouse, direction); 
-        if (Physics.Raycast(mouse, direction, out hit)){
-            var target = hit.transform.gameObject;
-            var resource = target.GetComponent<SelectableResource>();
-            if (resources.Count <= 0) Select(resource);
-            else if (CheckSelect(resource)) Select(resource); 
+    private void Update() {
+        if (Input.GetMouseButtonUp(0)){ 
+            canSelect = false;
+            DeselectAll();
+        }
+        if (Input.GetMouseButtonDown(0)){
+            canSelect = true; 
         }
     }
 
-    private void Select(SelectableResource res){
-        if (!resources.Contains(res)) resources.Add(res);
-        res?.Select();
+    private void DeselectAll(){
+        foreach (var resource in selectedResources)
+            resource.Deselect();
     }
 
-    private void BeforeSelect(){
-        resources = new List<SelectableResource>(); //Зануляем список
-        foreach (var i in resources) 
-            if (i != null) i.Deselect();
-        mousePositionSinceClick = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+    private void SelectResource(SelectableResource resource){
+        if (canSelect){ 
+            resource.Select();
+            selectedResources.Add(resource);
+        }
     }
 
-    private void AfterSelect(){
-        foreach (var i in resources) Destroy(i.gameObject);
-    }
-
-    //Не принимает нулл 
-    private bool CheckSelect(SelectableResource currentResource){
-        var lastResource = resources[resources.Count - 1];
-        var positionDelta = (lastResource.gridPosition - currentResource.gridPosition);
-        if (Mathf.Abs(positionDelta.x) < 3f && Mathf.Abs(positionDelta.y) < 3f)
-            return lastResource.info.id == currentResource.info.id;
-        return false;
+    private void OnDestroy(){
+        foreach (var resource in resources)
+            resource.OnSelect -= SelectResource;
     }
 }
