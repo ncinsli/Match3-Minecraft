ec54f0274e14f417e703da639032ceaece8292a1
SimpleLogicCompany <simplelogiccompany@gmail.com>
2020-12-13 16:49:33 +0200

    Move Unity Projects Folder Updard To GIT Root Folder. Add new .gitignore and .gitattributes. Remove ResourceSpawner.cs script and move code to BattleGrid.cs. Refactor BattleGrid.cs, Selector.cs, SelectableResource.cs, InventoryElement.cs Add new Sprites from Test(Ssprites not Complete)

--- Match3-Minecraft/Assets/Scripts/Battle3/Interfaces and Abstractions/InventoryElement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "", menuName = "Game logic/Inventory Element", order = 15)]
-public class InventoryElement : ScriptableObject {
-    [SerializeField] public int id;
-    [SerializeField] public string name;
-    [SerializeField] public Sprite texture;
-}
